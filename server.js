7c73385b664c3e637bc56fa46130b8c6d667f9eb
SHY-KG <tjdghks3078etc@gmail.com>
2020-12-17 22:00:17 +0900

    Get-Access_token

--- server.js
+++ server.js
@@ -14,5 +14,28 @@ app.get('/login/github', (req,res) => {
     res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=read:user`)
 })
 
+let token = null;
+
+app.get('/login/github/callback', (req, res) => {
+  const body = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    code: req.query.code
+  };
+
+  let opts = { headers: { accept: 'application/json' } };
+
+  axios.post(`https://github.com/login/oauth/access_token`, body, opts).
+    then(res => res.data['access_token']).
+    then(_token => {
+      token = _token;
+      console.log('token:', token);
+      res.json({ ok: Success });
+    }).
+    catch(err => res.status(500).json({ message: err.message }));
+
+});
+
+
 app.listen(5000);
 console.log('http://localhost:5000')
